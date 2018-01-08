0e238b6c8aa5d6acaa0793ce4feeccd3d6067470
cedricmart8 <cedricmart8@gmail.com>
2018-01-08 18:04:10 +0100

    ajout effect plus 1 d attaque a tout le monde

---
+++ src/card/effect/EffectPlus1AttaqueToutServiteur.java
@@ -0,0 +1,36 @@
+package card.effect;
+
+public class EffectPlus1AttaqueToutServiteur extends EffectDecorator {
+	private String name;
+	private String effect;
+	
+	public EffectPlus1AttaqueToutServiteur() {
+		this.name = "Plus 1 d attaque";
+		this.effect = "Plus 1 d attaque a tous les serviteurs alliés sur le plateau (tant que ce serviteur est vivant)";
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+
+	@Override
+	public String getEffect() {
+		// TODO Auto-generated method stub
+		return this.effect;
+	}
+
+	@Override
+	public void useEffect() { //donne a tout les serviteurs 1 point d attaque en plus pendant que ce serviteur est en vie 
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Cette carte possede un effet de "+this.getName()+".\n"+
+			this.effect;
+	}
+
+}
