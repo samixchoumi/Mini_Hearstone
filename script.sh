264f9f03c696c59109134efb656f10ba22ffab53
cedricmart8 <cedricmart8@gmail.com>
2018-01-12 22:13:42 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
+# telechargement du code source depuis git 
 git clone https://github.com/samixchoumi/Mini_Hearstone.git
 
+# compile programme 
 javac Mini_Hearstone/src/card/FactoryCard.java Mini_Hearstone/src/card/Observable.java Mini_Hearstone/src/card/commune/*.java Mini_Hearstone/src/card/effect/*.java Mini_Hearstone/src/card/guerrier/*.java Mini_Hearstone/src/card/mage/*.java Mini_Hearstone/src/card/paladin/*.java Mini_Hearstone/src/game/*.java Mini_Hearstone/src/hero/*.java Mini_Hearstone/src/mecanique/*.java Mini_Hearstone/src/power/*.java
 
+# execute mains
 java Mini_Hearstone.src.game.Main
