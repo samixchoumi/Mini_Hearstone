d906ae95913cf8bf3c28d142a558170327f03ca6
cedricmart8 <cedricmart8@gmail.com>
2018-01-12 22:08:58 +0100

    script

---
+++ script.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/samixchoumi/Mini_Hearstone.git
+
+javac Mini_Hearstone/src/card/FactoryCard.java Mini_Hearstone/src/card/Observable.java Mini_Hearstone/src/card/commune/*.java Mini_Hearstone/src/card/effect/*.java Mini_Hearstone/src/card/guerrier/*.java Mini_Hearstone/src/card/mage/*.java Mini_Hearstone/src/card/paladin/*.java Mini_Hearstone/src/game/*.java Mini_Hearstone/src/hero/*.java Mini_Hearstone/src/mecanique/*.java Mini_Hearstone/src/power/*.java
+
+java Mini_Hearstone.src.game.Main
