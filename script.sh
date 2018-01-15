d8d203c345937ef0060844eb2b1aa89b55679de8
samixchoumi <samixchoumi@users.noreply.github.com>
2018-01-15 10:47:46 +0100

    update script

--- script.sh
+++ script.sh
@@ -1,8 +1,13 @@
 # telechargement du code source depuis git 
+clear
 git clone https://github.com/samixchoumi/Mini_Hearstone.git
+clear
 
 # compile programme 
-javac Mini_Hearstone/src/card/FactoryCard.java Mini_Hearstone/src/card/Observable.java Mini_Hearstone/src/card/commune/*.java Mini_Hearstone/src/card/effect/*.java Mini_Hearstone/src/card/guerrier/*.java Mini_Hearstone/src/card/mage/*.java Mini_Hearstone/src/card/paladin/*.java Mini_Hearstone/src/game/*.java Mini_Hearstone/src/hero/*.java Mini_Hearstone/src/mecanique/*.java Mini_Hearstone/src/power/*.java
+mkdir Mini_Hearstone/build
+javac -d Mini_Hearstone/build Mini_Hearstone/src/Main.java Mini_Hearstone/src/card/FactoryCard.java Mini_Hearstone/src/card/Observable.java Mini_Hearstone/src/card/commune/*.java Mini_Hearstone/src/card/effect/*.java Mini_Hearstone/src/card/guerrier/*.java Mini_Hearstone/src/card/mage/*.java Mini_Hearstone/src/card/paladin/*.java Mini_Hearstone/src/game/*.java Mini_Hearstone/src/hero/*.java Mini_Hearstone/src/mecanique/*.java Mini_Hearstone/src/power/*.java
+clear
 
 # execute main
-java Mini_Hearstone.src.game.Main
+cd Mini_Hearstone
+java -cp build/ Main
